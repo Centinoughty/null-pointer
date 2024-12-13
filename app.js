cc55612a30318ef2298f7237c444421590a451d0
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-14 03:52:11 +0530

    creted database config

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
+const { connectDb } = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
+const UPLOAD_DIR = path.join(__dirname, "uploads");
 
+connectDb();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
