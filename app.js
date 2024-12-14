9e003c0047ec588fdc620419cf2060d677ff27bd
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-14 09:04:35 +0530

    created file route file

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const path = require("path");
 const { connectDb } = require("./config/db");
 require("dotenv").config();
 
+const fileRoute = require("./route/file");
+
 const app = express();
 const PORT = process.env.PORT;
 const UPLOAD_DIR = path.join(__dirname, "uploads");
+
+app.use("/api/files", fileRoute);
 
 connectDb();
 app.listen(PORT, () => {
