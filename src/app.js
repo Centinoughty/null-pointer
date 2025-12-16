31d57b585f300a81fa62ea1b729c8121ac07fed7
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-12-16 11:47:40 +0530

    created uploads dir

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const fs = require("fs");
+const crypto = require("crypto");
 require("dotenv").config({ path: ".env" });
 
 const app = express();
 const PORT = process.env.PORT;
+const UPLOAD_DIR = "uploads";
+
+if (!fs.existsSync(UPLOAD_DIR)) {
+  fs.mkdirSync(UPLOAD_DIR);
+}
+
+const generateId = () => crypto.randomBytes(3).toString("hex");
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
